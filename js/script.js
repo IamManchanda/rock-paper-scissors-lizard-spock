e4555953f6adc242575d4d64ec0a155e5d0547e2
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-09 20:45:23 +0530

    more progress in game

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+const playerScoreEl = document.getElementById("player-score");
+const playerChoiceEl = document.getElementById("player-choice");
+const playerRock = document.getElementById("player-rock");
+const playerPaper = document.getElementById("player-paper");
+const playerScissors = document.getElementById("player-scissors");
+const playerLizard = document.getElementById("player-lizard");
+const playerSpock = document.getElementById("player-spock");
+const computerScoreEl = document.getElementById("computer-score");
+const computerChoiceEl = document.getElementById("computer-choice");
+const computerRock = document.getElementById("computer-rock");
+const computerPaper = document.getElementById("computer-paper");
+const computerScissors = document.getElementById("computer-scissors");
+const computerLizard = document.getElementById("computer-lizard");
+const computerSpock = document.getElementById("computer-spock");
+const resultText = document.getElementById("result-text");
+const reset = document.getElementById("reset");
+const allGameIcons = document.querySelector(".far");
+
 const choices = {
   rock: {
     name: "Rock",
